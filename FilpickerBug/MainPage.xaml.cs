4b74a21a16689948e5f7d45335eeb56d39d457d9
bhavanesh2001 <bhavanesh001@gmail.com>
2025-02-11 23:54:01 +0530

    first commit

---
+++ FilpickerBug/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace FilpickerBug;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+
+    private async void CounterBtn_Clicked(object sender, EventArgs e)
+    {
+		var res = await FilePicker.PickMultipleAsync();
+		myLabel.Text = $"{res.Count()} files selected.";
+    }
+}
+
---
+++ FilpickerBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FilpickerBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
